17a92b4827f5e34c7b08c00505890e97b1d7c6d6
Tharusan Vijayakumar <tharusan.vijayakumar@gmail.com>
2024-12-11 11:23:03 +0000

    Jump

--- Assets/PlayerStuff/PlayerController.cs
+++ Assets/PlayerStuff/PlayerController.cs
@@ -8,27 +8,56 @@ public class PlayerController : MonoBehaviour
 {
     public float movementSpeed = 5f;
     public float jumpForce = 16f;
+
+    public int amountOfJumps = 1;
+    private int amountOfJumpsLeft;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+
+    private bool canMove;
+    private bool canJump;
+
     private float movementInputDirection;
 
 
+    public Transform groundCheckObj;
     private Rigidbody2D rb;
+    private Animator animator;
+
+    private bool isMove = false;
+    private bool isKnifeAttack = false;
+    private bool isGrounded = false;
 
     private void Awake(){
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Start is called before the first frame update
     void Start(){
-        
+        amountOfJumpsLeft = amountOfJumps;
     }
 
     // Update is called once per frame
     void Update(){
         CheckInput();
+        CheckMovementDirection();
+        UpdateAnimator();
+        CheckIfCanJump();
     }
 
     private void FixedUpdate(){
         ApplyMovement();
+        CheckSurroundings();
+    }
+
+    private void CheckMovementDirection(){
+        if(rb.velocity.x != 0){
+            isMove = true;
+        }
+        else{
+            isMove = false;
+        }
     }
 
     private void CheckInput(){
@@ -44,7 +73,35 @@ public class PlayerController : MonoBehaviour
     }
 
     private void Jump(){
-        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        if(canJump){
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            amountOfJumpsLeft--;
+        }
     }
 
+    private void UpdateAnimator(){
+        animator.SetBool("isMove", isMove);
+    }
+
+    private void CheckSurroundings(){
+        isGrounded = Physics2D.OverlapCircle(groundCheckObj.position, groundCheckRadius, whatIsGround);
+    }
+
+    
+    private void OnDrawGizmos (){
+        Gizmos.DrawWireSphere(groundCheckObj.position, groundCheckRadius);
+    }
+
+    private void CheckIfCanJump(){
+        if(isGrounded && rb.velocity.y <= 0){
+            amountOfJumpsLeft = amountOfJumps;
+        }
+
+        if(amountOfJumpsLeft <= 0){
+            canJump = false;
+        }
+        else{
+            canJump = true;
+        }
+    }
 }
